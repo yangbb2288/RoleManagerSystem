dce066ab7b9441a1a388e40c6c5e62a99a6da4bb
yangbb2288 <2904486018@qq.com>
2025-11-19 12:30:51 +0800

    日志跟新

--- src/main/java/com/yang/controller/AddTableController.java
+++ src/main/java/com/yang/controller/AddTableController.java
@@ -5,11 +5,15 @@ import com.yang.entity.Role;
 import com.yang.entity.result.Result;
 import com.yang.service.AddTableServiceIO;
 import com.yang.service.impl.AddTableService;
+import org.apache.log4j.Logger;
 
 public class AddTableController {
     AddTableServiceIO addTable=new AddTableService();
+    private static final Logger logger = Logger.getLogger(AddTableController.class);
 
     public Result addTable(Permission permission,Role role){
+        logger.info("添加权限表:");
+        logger.info("权限信息："+ permission+"角色信息："+role);
         try{
             if(addTable.addTable(permission,role)){
                 return new Result(200,"添加成功",null);
@@ -24,10 +28,13 @@ public class AddTableController {
         }
     }
     public Result deleteTable(Permission permission,Role role){
+        logger.info("删除权限表:");
+        logger.info("权限信息："+ permission+"角色信息："+role);
         try{
             if(addTable.deleteTable(permission,role)){
                 return new Result(200,"删除成功",null);
             }
+            logger.info("删除权限表失败");
             return new Result(400,"删除失败",null);
         }catch (Exception e){
             String message = e.toString();
